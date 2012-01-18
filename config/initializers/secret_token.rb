a7c7884de370d9eba8b77be1ff1836b1c662f1fc
Arzav Jain <arzavj@stanford.edu>
2012-01-18 14:53:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidactica::Application.config.secret_token = '4ccf59e8d908a7ffe6abe308b7eed8d99e801a4b9008ed57f3468cbaedf45fb422025ab3705fa3e9893e5319f1157cb81b81265996841495d673f777cb83cf99'
