c2635996cd4cb82fe404631c80961fe61c4c5daa
Arzav Jain <arzavj@stanford.edu>
2012-01-28 22:51:48 -0800

    fb button added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : 'YOUR_APP_ID',
+    status     : true, 
+    cookie     : true,
+    xfbml      : true,
+    oauth      : true,
+  });
+};
+(function(d){
+   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   d.getElementsByTagName('head')[0].appendChild(js);
+ }(document));
+
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
