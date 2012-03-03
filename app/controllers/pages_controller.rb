988d60a139cbb3dea0a27fcb9ed868ea8038d269
unknown <Suganty@.(none)>
2012-03-03 12:00:32 -0800

    Merge with github

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,4 +23,19 @@ class PagesController < ApplicationController
     @title = "View Top Video Sessions"
   end
 
+	def submitQuestion
+		q = Question.new
+		q.user_id = 1
+		q.question = params[:question]
+		q.category = Integer(params[:category])
+		save = q.save
+		Rails.cache.write("saved", save)
+		redirect_to "/pages/ask"
+	end
+
+	def getCategory
+		Rails.cache.write("Category", Integer(params[:category]))
+		redirect_to "/pages/give"
+	end
+
 end
