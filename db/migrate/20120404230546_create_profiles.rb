f804c22350abfe45cded4b8c8c82e63be396edad
unknown <Suganty@.(none)>
2012-04-07 00:09:49 -0700

    Trying to fix

--- db/migrate/20120404230546_create_profiles.rb
+++ db/migrate/20120404230546_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration
   def self.up
     create_table :profiles do |t|
-	t.has_attached_file :picture
+	t.has_attached_file "picture"
 	t.text "blurb"
       t.timestamps
     end
