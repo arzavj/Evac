36968262f954cac06d2c514d4aebdb3d6fe7f53a
VMohan <Vasanth@rescomp-11-221637.stanford.edu>
2012-04-20 16:12:12 -0700

    Possible bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Vidactica::Application.configure do
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
-x
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
