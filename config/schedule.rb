5f080a686093a3ea1b18941b6e02b9533a9772c3
VMohan <Vasanth@rescomp-11-221637.stanford.edu>
2012-05-09 21:45:18 -0700

    test

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,7 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
- every 1.day at  :at => '9:45 pm' do
+ every 1.day at  :at => '9:47 pm' do
 	 command "echo 'you can use raw cron syntax too'"
    runner "vid_mail.Reminder"
  end
