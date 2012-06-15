6af7467e068fa44af101cc6b7863c61066b20003
Vasanth Mohan <VasanthAdmin@Vasanths-MacBook-Pro.local>
2012-06-14 21:56:15 -0700

    Test 4

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,13 +1,13 @@
 ActionMailer::Base.smtp_settings = {
-    #:address => "127.0.0.1", 
-	:address              => "smtp.gmail.com",
-    #:port => 25,
-	:port => 587,
+    :address => "127.0.0.1", 
+	#:address              => "smtp.gmail.com",
+    :port => 25,
+	#:port => 587,
 	#:domain               => "domain.pl",
-    #:user_name => "noreply", 
-	:user_name => "noreplyvidactica",
-	#:password => "jS;v0Uf/#@oLKiE<y",
-	:password => "vidactica321",
+    :user_name => "noreply", 
+	#:user_name => "noreplyvidactica",
+	:password => "jS;v0Uf/#@oLKiE<y",
+	#:password => "vidactica321",
 	:authentication       => "plain",
 	:enable_starttls_auto => true
 }
