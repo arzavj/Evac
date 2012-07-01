55ac18db6993e16fe3850af5106d6768d94d7184
Vasanth Mohan <VasanthAdmin@Vasanths-MacBook-Pro.local>
2012-07-01 16:32:14 -0700

    Fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-	skip_before_filter :require_login, :only => :home, :privacyPolicy
+	skip_before_filter :require_login, :only => [:home, :privacyPolicy]
 	@@categories = ["Politics", "Philosophy", "Entertainment", "Business", "Social Justice", "Science", "Tutoring", "Sports", "Miscellaneous"]
 	
   def home
