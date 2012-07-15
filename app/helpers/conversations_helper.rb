3cbb6dd10cd6b75b74e1fb634b068a0e8fbaf93c
VMohan <Vasanth@Vasanths-MacBook-Pro.local>
2012-07-15 15:51:26 -0700

    Bug fixes and shorten time

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -14,4 +14,16 @@ module ConversationsHelper
 			@qConfirmed << q
 		end
 	end
+	
+	def calculateFutureDateForQuestion(q, s)
+		if q.first_entry.nil?
+			return s.appointment
+		else
+			if q.first_entry > s.appointment
+				return q.first_entry
+			else
+				return s.appointment
+			end
+		end
+	end
 end
