b2326a6506d44dbe696cb9c113f24f5029f48e33
VMohan <Vasanth@Vasanths-MacBook-Pro.local>
2012-07-22 19:54:53 -0700

    Change warnings and add missing files

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,13 +11,12 @@ class RegistrationsController < Devise::RegistrationsController
 				sign_in(resource_name, resource)
 				respond_with resource, :location => after_sign_up_path_for(resource)
 			else
-				set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
 				expire_session_data_after_sign_in!
 				respond_with resource, :location => after_inactive_sign_up_path_for(resource)
 			end
 		else
 			clean_up_passwords resource
-			flash[:error] = "Invalid email or password"
+			flash[:error] = "Please fill out your registration form properly"
 			redirect_to "/"
 		end
 	end
