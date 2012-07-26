23f0ed30872359d8ecce74c15a11ae526f512d2c
VMohan <Vasanth@Vasanths-MacBook-Pro.local>
2012-07-25 21:26:55 -0700

    Make it a migration

---
+++ db/migrate/20120726042523_migrat_users.rb
@@ -0,0 +1,15 @@
+class MigratUsers < ActiveRecord::Migration
+  def self.up
+	old_users = OldUser.all
+		old_users.each do |old_user|
+		devise_old_user = User.find(old_user.id)
+		devise_old_user.update_attributes(:password => old_user.password, :password_confirmation => old_user.password)
+	end
+
+	remove_column :users, :password
+  end
+
+  def self.down
+	add_column :users, :password, :string
+  end
+end
