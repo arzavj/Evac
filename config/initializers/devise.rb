3f736788189d2fc5ef85ea7fc35ba482740d4df1
VMohan <Vasanth@Vasanths-MacBook-Pro.local>
2012-07-28 15:39:41 -0700

    Order?

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,12 +1,12 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model
 Devise.setup do |config|
+	config.assets.initialize_on_precompile = false
+	
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
 	config.mailer_sender = "noreply@vidactica.com"
-	
-	config.assets.initialize_on_precompile = false
 	
 	config.warden do |manager|
 		manager.failure_app = CustomFailure
