6e4115b3bd3c308c6ffb6c386e29b9435c1daa98
VMohan <Vasanth@Vasanths-MacBook-Pro.local>
2012-08-21 19:37:27 -0700

    Test url

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,10 +2,14 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def facebook
 	#You need to implement the method below in your model
 		@user= User.find_for_facebook_oauth(request.env["omniauth.auth"],current_user)
-		
-		flash[:notice] = I18n.t"devise.omniauth_callbacks.success", :kind=>"Facebook"
-		sign_in_and_redirect @user, :event=>:authentication
-
+		if @user.persisted?
+			flash[:notice] = I18n.t"devise.omniauth_callbacks.success", :kind=>"Facebook"
+			sign_in_and_redirect @user, :event=>:authentication
+		else
+			session["devise.facebook_data"] = request.env["omniauth.auth"]
+			flash[:error] = "Authentication failed"
+			redirect_to root_url
+		end
 	end
 	
 	def passthru
