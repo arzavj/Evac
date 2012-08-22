959f79c703183c92585745c458cc341429669b71
VMohan <Vasanth@Vasanths-MacBook-Pro.local>
2012-08-21 19:30:14 -0700

    Uncomment

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,8 +3,8 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	#You need to implement the method below in your model
 		@user= User.find_for_facebook_oauth(request.env["omniauth.auth"],current_user)
 		
-		#flash[:notice] = I18n.t"devise.omniauth_callbacks.success", :kind=>"Facebook"
-		#sign_in_and_redirect @user, :event=>:authentication
+		flash[:notice] = I18n.t"devise.omniauth_callbacks.success", :kind=>"Facebook"
+		sign_in_and_redirect @user, :event=>:authentication
 
 	end
 	
