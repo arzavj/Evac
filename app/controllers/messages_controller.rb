365d49c7294ded6a19f91f303ab5184478262814
VMohan <Vasanth@rescomp-12-221637.stanford.edu>
2012-09-17 20:17:30 -0700

    Messages functinoal

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,13 @@ class MessagesController < ApplicationController
 		@conversations = current_user.mailbox.conversations
 	end
 	
-	def new 
-		
+	def create 
+		if params[:message][:notified_object_id]
+			current_user.send_message(User.find(params[:message][:notified_object_id]), params[:message][:body], params[:message][:subject])
+		elsif params[:message][:conversation_id]
+			current_user.reply_to_conversation(Conversation.find(params[:message][:conversation_id]), params[:message][:body])
+		end
+		redirect_to messages_url
 	end
+
 end
